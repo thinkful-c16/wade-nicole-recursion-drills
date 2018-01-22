76315b82f68467713f22f432b748d02fc0a614cd
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-01-22 10:12:45 -0800

    Added string splitter file, began work on recursion solution

---
+++ string-splitter.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function stringSplitter(str, sep) {
+  if (str.length === 0){
+    return [];
+  }
+  else {
+    
+  }
+}
+
+let string = "red/orange/yellow/green/blue/purple";
+let separator = "/";
